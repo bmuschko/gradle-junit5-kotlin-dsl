bcf02eafa4b8693e20d40811ddb6ed038802d92d
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-04-11 17:06:25 -0600

    Create dedicated source set and task for functional tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
+
 plugins {
     `java-gradle-plugin`
     kotlin("jvm") version "1.2.31"
@@ -37,3 +39,28 @@ val test by tasks.getting(Test::class) {
         includeEngines("spek")
     }
 }
+
+val sourceSets = java.sourceSets
+
+val functionalTestSourceSet by sourceSets.creating {
+    withConvention(KotlinSourceSet::class) {
+        kotlin.srcDir("src/functTest/kotlin")
+    }
+    resources.srcDir("src/functTest/resources")
+    compileClasspath += sourceSets["main"]!!.output + configurations.testRuntimeClasspath
+    runtimeClasspath += output + compileClasspath
+}
+
+val functionalTest by tasks.creating(Test::class) {
+    description = "Runs the functional tests"
+    group = "verification"
+    testClassesDirs = functionalTestSourceSet.output.classesDirs
+    classpath = functionalTestSourceSet.runtimeClasspath
+    mustRunAfter("test")
+    reports {
+        html.destination = file("${html.destination}/functional")
+        junitXml.destination = file("${junitXml.destination}/functional")
+    }
+}
+
+tasks.getByName("check").dependsOn(functionalTest)
